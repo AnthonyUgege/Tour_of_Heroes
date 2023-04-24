f170c7cdcc5d118cbfa6d2df42633e765acc1f9c
Anthony Omoike Ugege <ugegeao@basfad.basf.net>
2023-04-24 23:59:39 +0200

    second commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,22 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 3, name: 'Tomas' },
+    { id: 4, name: 'Agustin' },
+    { id: 5, name: 'Corinna' },
+    { id: 6, name: 'Hoon' },
+    { id: 7, name: 'Joshua' },
+    { id: 8, name: 'Marcell' },
+    { id: 9, name: 'Ruben' },
+    { id: 10, name: 'Leander' },
+    { id: 11, name: 'Anthony' },
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
